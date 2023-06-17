2ea2b21a081e2792a81280f43ff9148995811d34
Eito Takase <eito01178@gmail.com>
2023-06-17 12:51:14 +0900

    scriptを最速で読み込む

--- src/core/templates/index.ts
+++ src/core/templates/index.ts
@@ -1,24 +1,8 @@
 import config from "../../config";
 
 export const browserTesterScriptTemplate = `
-<script id="__bt_script__">//<![CDATA[
-  (function() {
-    try {
-      var script = document.createElement('script');
-      if ('async') {
-        script.async = true;
-      }
-      script.src = 'http://HOST:${config.proxy.port}/browser-tester-static/index.js?t=TIME'.replace("HOST", location.hostname).replace("TIME", Date.now());
-      if (document.body) {
-        document.body.appendChild(script);
-      } else if (document.head) {
-        document.head.appendChild(script);
-      }
-    } catch (e) {
-      console.error("Browsertester: could not append script tag", e);
-    }
-  })()
-//]]></script>`;
+<script src="/browser-tester-static/index.js?v=0"></script>
+`;
 
 export const browserSyncScriptTemplate = `
 <script id="__bs_script__">//<![CDATA[
